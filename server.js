cfb3cbb42a7e1d0940671f50f748bc6b5d1ed268
SlayerisDead45 <havidharshan8c@gmail.com>
2026-06-06 03:35:09 +0530

    fix css loading

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
-// i am using built in modules, no install needed
 var http = require("http");
 var fs = require("fs");
 
 // this function reads and sends html files
 function sendPage(res, filename, statusCode) {
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            res.end("Server error");
-            return;
-        }
-        // sending html back to browser
-        res.writeHead(statusCode, { "Content-Type": "text/html" });
-        res.end(data);
-    });
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(500);
+      res.end("Server error");
+      return;
+    }
+    // sending html back to browser
+    res.writeHead(statusCode, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 }
 
 // i am creating the server here
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
 
-    if (req.url === "/" || req.url === "/home") {
-        sendPage(res, "index.html", 200);
+  // serving css and js files
+  if (req.url === "/style.css") {
+    fs.readFile("style.css", function(err, data) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
+    return;
+  }
 
-    } else if (req.url === "/about") {
-        sendPage(res, "about.html", 200);
+  if (req.url === "/script.js") {
+    fs.readFile("script.js", function(err, data) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.end(data);
+    });
+    return;
+  }
 
-    } else if (req.url === "/contact") {
-        sendPage(res, "contact.html", 200);
+  // routes
+  if (req.url === "/" || req.url === "/home") {
+    sendPage(res, "home.html", 200);
 
-    } else {
-        // anything else gets a 404
-        sendPage(res, "404.html", 404);
-    }
+  } else if (req.url === "/about") {
+    sendPage(res, "about.html", 200);
+
+  } else if (req.url === "/contact") {
+    sendPage(res, "contact.html", 200);
+
+  } else {
+    // anything else gets a 404
+    sendPage(res, "404.html", 404);
+  }
 
 });
 
 var port = 3000;
-server.listen(port, function () {
-    console.log("server running on http://localhost:" + port);
+server.listen(port, function() {
+  console.log("server running on http://localhost:" + port);
 });
